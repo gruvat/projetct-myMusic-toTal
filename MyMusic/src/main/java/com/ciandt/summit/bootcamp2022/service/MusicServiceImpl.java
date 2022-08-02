425c4aeeb8ab7a25b3565889b0f13189f30b2b6c
julianacit <julianamt@ciandt.com>
2022-08-02 17:22:02 -0300

    Creating logging for Music Service.
    Co-authored-by: EdCiandt <luises@ciandt.com>

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicServiceImpl.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/service/MusicServiceImpl.java
@@ -4,13 +4,15 @@ import com.ciandt.summit.bootcamp2022.common.exception.service.InvalidParameterE
 import com.ciandt.summit.bootcamp2022.common.exception.service.MusicsAndArtistsNotFoundException;
 import com.ciandt.summit.bootcamp2022.entity.Music;
 import com.ciandt.summit.bootcamp2022.repository.MusicRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Set;
 
 @Service
+@Log4j2
 public class MusicServiceImpl implements MusicService {
 
     @Autowired
@@ -18,6 +20,9 @@ public class MusicServiceImpl implements MusicService {
 
     @Override
     public boolean isAValidSearch(String filter) {
+
+        log.info("Checking if filter length is bigger than 2");
+
         return filter.length() >= 2;
     }
 
@@ -25,15 +30,24 @@ public class MusicServiceImpl implements MusicService {
     @Cacheable(value = "music_cache")
     public Set<Music> searchMusicsByFilter(String filter) {
 
+        log.info("Entering music search by filter");
+
         if(isAValidSearch(filter)){
             Set<Music> musics = musicRepository.findMusicsByMusicsAndArtistsName(filter.toLowerCase());
 
             if (musics.isEmpty()) {
+
+                log.info("No results for filter {}", filter);
+
                 throw new MusicsAndArtistsNotFoundException();
             }
 
+            log.info("Successful search");
+
             return musics;
         }
+
+        log.error("Filter is invalid");
 
         throw new InvalidParameterException("The filter must have at least 2 characters.");
 
@@ -42,12 +56,20 @@ public class MusicServiceImpl implements MusicService {
     @Override
     @Cacheable(value = "music_cache")
     public Set<Music> searchAllMusics(){
+
+        log.info("Entering music search without filter");
+
         Set<Music> allMusics = musicRepository.findMusicsByMusicsAndArtistsName("");
 
         if(allMusics.isEmpty()){
+
+            log.info("There are no musics in the database");
+
             throw new MusicsAndArtistsNotFoundException();
 
         }
+
+        log.info("Successful search");
 
         return allMusics;
     }
