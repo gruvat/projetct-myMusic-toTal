efedb68d086737db966a0586cb9361068d7c533f
v-giorgio7 <vgiorgio@ciandt.com>
2022-08-11 11:05:45 -0300

    dto method corrected
    Co-authored-by: Lorenza Silveira <105365688+lorenzas@users.noreply.github.com>

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/dto/MusicDto.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/controller/dto/MusicDto.java
@@ -29,7 +29,7 @@ public class MusicDto {
 
     public static Music toMusic(MusicDto musicDto) {
         Music music = new Music(musicDto.getId(), musicDto.getName());
-        music.setArtist(new Artist(music.getArtist().getId(), music.getArtist().getName()));
+        music.setArtist(new Artist(musicDto.getArtist().getId(), musicDto.getArtist().getName()));
         return music;
     }
 
